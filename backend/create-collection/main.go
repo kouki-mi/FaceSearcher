82e1f8ad7866f123d2479b2197aedec3ca62dc70
koki-miu <>
2024-01-02 15:52:24 +0900

    apiの仕様をまとめたopenAPIのyamlを作成

--- backend/create-collection/main.go
+++ backend/create-collection/main.go
@@ -4,15 +4,67 @@ import (
 	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/rekognition"
+	"encoding/json"
 )
 
+//リクエストボディ
+type MyRequestBody struct {
+	CollectionId string `json:"collectionId"`
+}
+
+// awsRekognitionのコレクションを作成する
+func createCollection(svc *rekognition.Rekognition, collectionId string) (*rekognition.CreateCollectionOutput, error){
+	// コレクションを作成する
+	input := &rekognition.CreateCollectionInput{
+		CollectionId: aws.String(collectionId),
+	}
+
+	result, err := svc.CreateCollection(input)
+	if err != nil {
+		fmt.Println(err)
+		return result, err
+	}
+	fmt.Println(result)
+	return result, nil
+}
+
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	//postで受け取ったjsonを表示
-	fmt.Println(request.Body);
+	// リクエストボディからコレクションIDを取得
+	requestBody := request.Body
+	var myRequestBody MyRequestBody
+	err := json.Unmarshal([]byte(requestBody), &myRequestBody) 
+	if err != nil {
+		fmt.Println(err)
+		return events.APIGatewayProxyResponse{
+			Body:       err.Error(),
+			StatusCode: 500,
+		}, nil
+	}
+
+	// セッションを作成
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	// Rekognitionサービスのクライアントを作成
+	svc := rekognition.New(sess, aws.NewConfig().WithRegion("ap-northeast-1"))
+	result, err := createCollection(svc, myRequestBody.CollectionId)
+
+	if err != nil {
+		fmt.Println(err)
+		return events.APIGatewayProxyResponse{
+			Body:       err.Error(),
+			StatusCode: 500,
+		}, nil
+	}
 	return events.APIGatewayProxyResponse{
-		Body:       "Hello, world!\n",
+		Body:       result.String(),
 		StatusCode: 200,
 	}, nil
+
 }
 
 func main() {
--- backend/go.mod
+++ backend/go.mod
@@ -2,4 +2,9 @@ module github.com/kouki-mi/faceSearcher
 
 go 1.21.0
 
-require github.com/aws/aws-lambda-go v1.42.0
+require (
+	github.com/aws/aws-lambda-go v1.42.0
+	github.com/aws/aws-sdk-go v1.49.4
+)
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
